3dfc3942ce978b315e90d8026a4e463856f9b030
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-09-21 07:00:43 +0000

    004

---
+++ 00_04_holamon/HolaMon.java
@@ -0,0 +1,9 @@
+/*
+estoy describiendo el primer programa de java de hola mon
+*/ 
+
+public class HolaMon {
+    public static void main(String[] args) {
+        System.out.println("Hola Món!");
+    }
+}
