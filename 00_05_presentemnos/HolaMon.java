b2673ab2722830cdaf1a836d47b29a9e0f2047b9
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-09-22 10:10:20 +0000

    odio el prgtest

---
+++ 00_05_presentemnos/HolaMon.java
@@ -0,0 +1,7 @@
+/* esto es un archivo de presentacion */
+
+public class HolaMon {
+    public static void main(String[] args) {
+        System.out.println("Hola, sóc d, la futura millor desenvolupadora del Món!");
+    }
+}
