cbbc6da7ec639d38508ec07c4f8387274f9cd298
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-10-08 14:32:08 +0000

    el java yo lo dejava

--- 11_02_dosenordre/DosEnOrdre.java
+++ 11_02_dosenordre/DosEnOrdre.java
@@ -6,6 +6,7 @@ public class DosEnOrdre {
     public static void main(String[] args) {
         System.out.println("Primer?");
         int primer = Integer.parseInt(Entrada.readLine());
+        System.out.println("Segon?");
         int segon = Integer.parseInt(Entrada.readLine());
 
         if (primer > segon) {
