9427c888ba12af6163911c73a76418ad59d4705f
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-10-13 08:40:45 +0000

    el java yo lo dejava

--- 11_11_tresenordre/TresEnOrdre.java
+++ 11_11_tresenordre/TresEnOrdre.java
@@ -2,21 +2,19 @@
 /**
  * Desenvolupa un programa que demani tres números i escrigui els tres números ordenats creixentment (de menor a major).
  */
-
 public class TresEnOrdre {
 
     public static void main(String[] args) {
         //primer?
         System.out.println("Primer?");
         int primer = Integer.parseInt(Entrada.readLine());
-        
+
         System.out.println("Segon?");//segon?
-        int segon = Integer.parseInt(Entrada.readLine()); 
-        
+        int segon = Integer.parseInt(Entrada.readLine());
+
         System.out.println("Tercer?"); //tercer?
         int tercer = Integer.parseInt(Entrada.readLine());
-        
-        // Find the smallest number
+
         int menor = primer;
         if (segon < menor) {
             menor = segon;
@@ -24,8 +22,6 @@ public class TresEnOrdre {
         if (tercer < menor) {
             menor = tercer;
         }
-        
-        // Find the largest number
         int mayor = primer;
         if (segon > mayor) {
             mayor = segon;
@@ -33,11 +29,9 @@ public class TresEnOrdre {
         if (tercer > mayor) {
             mayor = tercer;
         }
-        
-        // Find the middle number
         int medio = primer + segon + tercer - menor - mayor;
-        
+
         System.out.println(menor + ", " + medio + " i " + mayor);
-    
+
     }
 }
