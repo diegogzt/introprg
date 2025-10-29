8849036cdb112a892d67605d2721aaeb19634ba4
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-10-29 09:04:36 +0000

    el java yo lo dejava

---
+++ 12_08_analisi_caracter/AnalitzaCaracter.java
@@ -0,0 +1,31 @@
+public class AnalitzaCaracter {
+    public static void main(String[] args) {
+        System.out.println("Text?");
+        String text = Entrada.readLine();
+
+        if (text.isBlank() || text.length() < 1 || text.isEmpty()) {
+            System.out.println("Text buit");
+        } else {
+            System.out.println("Posicio?");
+            int n = Integer.parseInt(Entrada.readLine());
+            
+            // Ajustar la posición según las reglas del enunciado
+            int posicio = n % text.length();
+            if (posicio < 0) {
+                posicio = text.length() + posicio;
+            }
+            
+            char c = text.charAt(posicio);
+            
+            if (Character.isDigit(c)) {
+                System.out.println("'" + c + "'" + " és un dígit");
+            } else if (Character.isLowerCase(c)) {
+                System.out.println("'" + c + "'" + " és una lletra minúscula");
+            } else if (Character.isUpperCase(c)) {
+                System.out.println("'" + c + "'" + " és una lletra majúscula");
+            } else {
+                System.out.println("'" + c + "'" + " és altre");
+            }
+        }
+    }
+}
