8d0834fa2ad48c1b779e23e1c6105cf62a7073a8
Diego Gabriel Zaldivar Tovar <tovard799@gmail.com>
2025-10-29 09:10:48 +0000

    el java yo lo dejava

--- 12_08_analisi_caracter/AnalitzaCaracter.java
+++ 12_08_analisi_caracter/AnalitzaCaracter.java
@@ -1,3 +1,43 @@
+/*
+Enunciat
+
+Desenvolupa un programa anomenat AnalitzaCaracter que demani un text i una posició.
+
+Si el text és buit, indicarà "Text buit" i finalitzarà sense demanar res més.
+
+Si la posició està dins del rang de caracters del text introduït, considerarà el caracter corresponent a la posició. Per exemple, si la paraula és "hola" i la posició és 2, el caràcter corresponent serà 'l'.
+
+h o l a
+    ^
+0 1 2
+Si la posició supera el nombre de caràcters del text, continuarà comptant a partir del primer. Per exemple, si la paraula és "hola" i la posició és 6, el caràcter corresponent serà 'l'.
+
+h o l a
+    ^
+0 1 2 3         // continua comptant per l'inici
+4 5 6
+Si la posició és negativa, començarà a comptar a partir de l'últim caràcter del text introduït. Per exemple, -2 correspondrà a l'últim caràcter.
+
+h o l a
+    ^
+     -1         // comença pel final
+   -2           // compta decrementant
+Un cop identificat el caràcter escollit, el programa indicarà:
+
+el caràcter que correspon a la posició demanada
+
+la categoria del caràcter:
+
+lletra majúscula
+
+lletra minúscula
+
+dígit
+
+altre: quan no sigui cap dels anteriors
+
+Ja hem vist les utilitats de caràcters a un exercici anterior. Aquí en farem servir només algunes.
+*/
 public class AnalitzaCaracter {
     public static void main(String[] args) {
         System.out.println("Text?");
